08b57ba03e7bca6715e06f62b1b83f9c34e09282
dadanugm <dadanugm07@gmail.com>
2021-11-20 17:01:02 +0700

    initial commit
    initial commit

--- maix_train-master/instance/config.py
+++ maix_train-master/instance/config.py
@@ -12,7 +12,7 @@ allow_cpu = True # True
 
 # classifier
 #classifier_train_gpu_mem_require = 2*1024*1024*1024
-classifier_train_epochs = 50
+classifier_train_epochs = 10
 classifier_train_batch_size = 5
 classifier_train_max_classes_num = 15
 classifier_train_one_class_min_img_num = 40            # 一个类别中至少需要的样本数量
@@ -21,7 +21,7 @@ classifier_result_file_name_prefix = "maixhub_classifier_result"
 
 # detector
 #detector_train_gpu_mem_require = 2*1024*1024*1024
-detector_train_epochs = 50
+detector_train_epochs = 10
 detector_train_batch_size = 5
 detector_train_learn_rate = 1e-4
 detector_train_max_classes_num = 15         # 最多能训练多少类
