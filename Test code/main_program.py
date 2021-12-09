e6eb2df8690743d98af2beb9a85e51c6f1803465
dadanugm <dadanugm07@gmail.com>
2021-12-09 23:19:35 +0700

    initial commit
    initial commit

--- Test code/main_program.py
+++ Test code/main_program.py
@@ -12,19 +12,29 @@ import ujson
 from machine import UART
 
 fm.register(7, fm.fpioa.GPIOHS3, force=True)
-fm.register(12, fm.fpioa.UART2_TX, force=True)
-fm.register(13, fm.fpioa.UART2_RX, force=True)
+fm.register(6, fm.fpioa.GPIO6, force=True)
+#fm.register(5, fm.fpioa.UART1_TX, force=True)
+#fm.register(4, fm.fpioa.UART1_RX, force=True)
 
 gpio7 = GPIO(GPIO.GPIOHS3, GPIO.OUT)
-uart_esp = UART(UART.UART2, 115200, 8, 0, 0, timeout=1000, read_buf_len=4096)
-dht = DHT11(gpio)
+sw_esp = GPIO(GPIO.GPIO6, GPIO.OUT)
+#uart_esp = UART(UART.UART1, 115200, 8, 0, 0, timeout=1000, read_buf_len=4096)
+dht = DHT11(gpio7)
 
 
 def send_data(temp, hum, fire, smoke):
+    #switch on wifi and wait to connect
+    sw_esp.value(1)
+    time.sleep(5)
     #combine all data
     data = {"temp":temp,"hum":hum,"fire":fire,"smoke":smoke}
     payload = ujson.dumps(data)
-    uart_esp.write(payload)
+    print(payload)
+    #uart_esp.write(payload)
+    #turn off wifi
+    #time.sleep(2)
+    #sw_esp.value(0)
+    time.sleep(2)
 
 def main(anchors, labels = None, model_addr="/sd/m.kmodel", sensor_window=(224, 224), lcd_rotation=0, sensor_hmirror=False, sensor_vflip=False):
     sensor.reset()
@@ -35,12 +45,10 @@ def main(anchors, labels = None, model_addr="/sd/m.kmodel", sensor_window=(224, 
     sensor.set_vflip(sensor_vflip)
     sensor.run(1)
 
-    lcd.init(type=1)
-    lcd.rotation(lcd_rotation)
-    lcd.clear(lcd.WHITE)
-
-    fire_val = 0
-    smoke_val = 0
+    #lcd.init(type=1)
+    #lcd.rotation(lcd_rotation)
+    #lcd.clear(lcd.WHITE)
+    print("runn main")
 
     if not labels:
         with open('labels.txt','r') as f:
@@ -51,30 +59,44 @@ def main(anchors, labels = None, model_addr="/sd/m.kmodel", sensor_window=(224, 
         task = kpu.load(model_addr)
         kpu.init_yolo2(task, 0.3, 0.5, 5, anchors) # threshold:[0,1], nms_value: [0, 1]
         while 1:
-            img = sensor.snapshot()
+            fire_val = 0.0
+            smoke_val = 0.0
+            #img = sensor.snapshot()
+            img = image.Image("/sd/images2/fire_5.jpg")
             print("read image")
             #lcd.display(img)
+            img = img.resize(224,224)
+            img.pix_to_ai()
             objects = kpu.run_yolo2(task, img)
             if objects:
                 for obj in objects:
+                    fire_val_tmp = 0.0
+                    smoke_val_tmp = 0.0
                     pos = obj.rect()
                     img.draw_rectangle(pos)
                     img.draw_string(pos[0], pos[1], "%s : %.2f" %(labels[obj.classid()], obj.value()), scale=1, color=(255, 0, 0))
-                    if labels[obj.classid() == "fire":
-                        fire_val = obj.value()
-                    elif labels[obj.classid() == "smoke":
-                        smoke_val = obj.value()
-                    else:
-                        fire_val = 0
-                        smoke_val = 0
+                    print(obj.value())
+                    print(labels[obj.classid()])
+                    if labels[obj.classid()] == "fire":
+                        fire_val_tmp = obj.value()
+                        if fire_val_tmp > fire_val:
+                            fire_val = fire_val_tmp
+                    elif labels[obj.classid()] == "smoke":
+                        smoke_val_tmp = obj.value()
+                        if smoke_val_tmp > smoke_val:
+                            smoke_val = smoke_val_tmp
+
+
             #get data
             sensordht = dht.read()
+            #tempr = 10
+            #humid = 20
             tempr = sensordht.temperature
             humid = sensordht.humidity
             send_data(tempr,humid,fire_val,smoke_val)
             #img.draw_string(0, 200, img, scale=1, color=(255, 0, 0))
             #lcd.display(img)
-            time.sleep(1)
+            time.sleep(2)
     except Exception as e:
         raise e
     finally:
